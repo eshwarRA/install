0afdd2661cb2465af553a4b44c61878f7921b6bd
eshwarRA <67508481+eshwarRA@users.noreply.github.com>
2020-10-13 19:04:00 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ sudo apt install htop cmatrix mlocate
 #sudo apt install gparted emacs vlc virtualbox gimp synaptic snapd wine         
 #sudo apt install spyder3                                                       
 #sudo apt install cewl crunch nmap hydra hashcat john beef wireshark
-#curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
-# chmod 755 msfinstall && \
-# ./msfinstall
 mkdir programs
 cd programs
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
+ chmod 755 msfinstall && \
+ ./msfinstall
 mkdir kernel gnuCommands python java c projects
 cd 
-#sudo flatpak install BlueJ Spotify PyCharm-Community WhatsAppQT
+
