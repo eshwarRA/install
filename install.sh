ac7e3122b8a33dfe169d0b024779bfdfa437e692
eshwarRA <67508481+eshwarRA@users.noreply.github.com>
2020-10-15 18:07:05 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 sudo apt update
-sudo apt dist-upgrade
-sudo apt install code tilix gnome-tweaks google-chrome-stable atom ubuntu-restricted-extras tlp tlp-rdw timeshift 
+sudo apt dist-upgrade -y
+sudo apt install code tilix gnome-tweaks google-chrome-stable atom ubuntu-restricted-extras tlp tlp-rdw timeshift -y
 sudo update-alternatives --config x-terminal-emulator
-sudo apt install htop cmatrix mlocate youtube-dl preload bleachbit vim thunderbird 
-sudo apt install gparted emacs vlc virtualbox gimp synaptic snapd wine         
-sudo apt install spyder3                                                       
-sudo apt install cewl crunch nmap hydra hashcat john beef wireshark
+sudo apt install htop cmatrix mlocate youtube-dl preload bleachbit vim thunderbird -y 
+sudo apt install gparted emacs vlc virtualbox gimp synaptic snapd wine -y         
+sudo apt install spyder3 -y                                                       
+sudo apt install cewl crunch nmap hydra hashcat john beef wireshark -y
 mkdir programs
 cd programs
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
