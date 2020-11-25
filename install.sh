fd4d974a6d9afdd9f77e78d3ebd60431699d4f96
eshwarRA <67508481+eshwarRA@users.noreply.github.com>
2020-11-25 13:22:32 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,7 @@
 sudo apt update
 sudo apt dist-upgrade -y
-sudo apt install code tilix gnome-tweaks google-chrome-stable atom ubuntu-restricted-extras tlp tlp-rdw timeshift -y
-sudo update-alternatives --config x-terminal-emulator
+sudo apt install code terminator google-chrome-stable atom kubuntu-restricted-extras tlp tlp-rdw timeshift -y
 sudo apt install htop cmatrix mlocate youtube-dl preload bleachbit vim thunderbird -y 
 sudo apt install gparted emacs vlc virtualbox gimp synaptic snapd wine -y         
 sudo apt install spyder3 -y                                                       
 sudo apt install cewl crunch nmap hydra hashcat john beef wireshark -y
-mkdir programs
-cd programs
-curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
- chmod 755 msfinstall && \
- ./msfinstall
-mkdir kernel gnuCommands python java c projects
-cd 
