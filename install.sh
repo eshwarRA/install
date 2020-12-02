3493d9328a2ba4e24c70d69f6690f7af311144aa
eshwarRA <67508481+eshwarRA@users.noreply.github.com>
2020-12-02 11:47:44 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-sudo dnf update
-sudo dnf install fedora-workstation-repositories
-sudo dnf install code terminator google-chrome-stable atom kubuntu-restricted-extras tlp tlp-rdw timeshift -y
-sudo dnf install htop cmatrix mlocate youtube-dl preload bleachbit vim thunderbird -y 
-sudo dnf install gparted emacs vlc virtualbox gimp synaptic snapd wine -y         
-sudo dnf install spyder3 -y                                                       
-#sudo dnf install cewl crunch nmap hydra hashcat john beef wireshark -y
+sudo apt update
+sudo apt full-upgrade -y
+sudo apt install code terminator google-chrome-stable atom kubuntu-restricted-extras tlp tlp-rdw timeshift -y
+sudo apt install htop cmatrix mlocate youtube-dl preload bleachbit vim thunderbird -y 
+sudo apt install gparted emacs vlc virtualbox gimp synaptic snapd wine -y         
+sudo apt install spyder3 -y                                                       
+sudo apt install cewl crunch nmap hydra hashcat john beef wireshark -y
