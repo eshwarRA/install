16f40617f7e7353b414b76b49b7740a121035d26
eshwarRA <67508481+eshwarRA@users.noreply.github.com>
2020-12-13 20:34:57 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt full-upgrade -y
 sudo apt install code terminator google-chrome-stable atom kubuntu-restricted-extras tlp tlp-rdw timeshift -y
-sudo apt install htop cmatrix mlocate youtube-dl preload bleachbit vim thunderbird -y 
-sudo apt install gparted emacs vlc virtualbox gimp synaptic snapd wine -y         
+sudo apt install htop cmatrix mlocate youtube-dl preload bleachbit clamav vim thunderbird -y 
+sudo apt install gparted emacs vlc virtualbox gimp synaptic flatpak wine -y         
 sudo apt install spyder3 -y                                                       
 sudo apt install cewl crunch nmap hydra hashcat john beef wireshark -y
