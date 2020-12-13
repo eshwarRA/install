807f3381141d3fc06603da4fa5115531f047ce31
eshwarRA <67508481+eshwarRA@users.noreply.github.com>
2020-12-13 20:40:38 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@ sudo apt update
 sudo apt full-upgrade -y
 sudo apt install code terminator google-chrome-stable atom kubuntu-restricted-extras tlp tlp-rdw timeshift -y
 sudo apt install htop cmatrix mlocate youtube-dl preload bleachbit clamav vim thunderbird -y 
-sudo apt install gparted emacs vlc virtualbox gimp synaptic flatpak wine -y         
+sudo apt install gparted emacs vlc virtualbox gimp synaptic flatpak wine -y 
+sudo apt install plasma-discover-backend-flatpak -y
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo apt install spyder3 -y                                                       
 sudo apt install cewl crunch nmap hydra hashcat john beef wireshark -y
