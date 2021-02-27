55f249f98e95de5c3d1f7c68023c8cf5766c0863
eshwarRA <67508481+eshwarRA@users.noreply.github.com>
2021-02-27 14:18:11 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,5 @@ sudo apt install code terminator google-chrome-stable atom xubuntu-restricted-ad
 sudo apt install htop cmatrix mlocate preload bleachbit clamav vim vim-airline vim-airline-themes -y
 wget http://ftp.de.debian.org/debian/pool/main/y/youtube-dl/youtube-dl_2021.02.04.1-1_all.deb
 sudo apt install ./youtube-dl_2021.02.04.1-1_all.deb
-sudo apt install gparted vlc virtualbox gimp synaptic flatpak -y 
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo                 
-sudo apt install cewl crunch nmap hydra hashcat john beef wireshark -y
+sudo apt install gparted vlc virtualbox gimp synaptic -y                  
+sudo apt install cewl crunch nmap hydra hashcat john wireshark -y
